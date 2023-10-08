e720c34816611d6da2daf00cf91380905360ffd6
Joel Auterson <joel.auterson@gmail.com>
2023-10-08 13:06:24 +0100

    Add EpicVars blank

---
+++ desktop/src/com/bearwaves/eos4jsample/EpicVars.java
@@ -0,0 +1,11 @@
+package com.bearwaves.eos4jsample;
+
+class EpicVars {
+
+    static final String PRODUCT_ID = "";
+    static final String SANDBOX_ID = "";
+    static final String DEPLOYMENT_ID = "";
+    static final String CLIENT_ID = "";
+    static final String CLIENT_SECRET = "";
+
+}
