205f33f1ee3b86d3d5d467505e87c8e7a77e3c45
Joel Auterson <joel.auterson@gmail.com>
2023-10-08 12:49:42 +0100

    Load EOS natives

--- desktop/src/com/bearwaves/eos4jsample/DesktopLauncher.java
+++ desktop/src/com/bearwaves/eos4jsample/DesktopLauncher.java
@@ -2,14 +2,20 @@ package com.bearwaves.eos4jsample;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.bearwaves.eos4jsample.GdxGame;
+import com.bearwaves.eos4j.EOS;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("eos4j Sample");
-		new Lwjgl3Application(new GdxGame(), config);
-	}
+    public static void main(String[] arg) {
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setForegroundFPS(60);
+        config.setTitle("eos4j Sample");
+
+        // Load eos4j
+        if (!EOS.loadLibraries()) {
+            throw new RuntimeException("Couldn't load EOS libraries");
+        }
+
+        new Lwjgl3Application(new GdxGame(), config);
+    }
 }
